4a0a8c321b205a0d7502e473f1c414c75503aacd
Shahjee783 <mshahzadofc@gmail.com>
2026-01-27 23:27:53 +0500

    Update MongoDB URI in config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ if (!gg) {
 // -------------------------------------------------------------- //
 
 global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "Put Here Your Mongodb URI";
+global.mongodb = process.env.MONGODB || "mongodb+srv://m0shahzad:m0shahzad@cluster0.iduwfwr.mongodb.net/?appName=Cluster0";
 global.sessionId = process.env.SESSION_ID || "Marin";
 global.prefa = process.env.PREFIX || "!";
 global.tenorApiKey = process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
